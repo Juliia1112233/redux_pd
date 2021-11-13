b89f16b168aa3fa33d45ac24867e3f1141399df5
Juliia1112233 <y.kvashchuk@gmail.com>
2021-11-13 22:12:44 +0200

    3 commit

--- src/Components/Counter/index.js
+++ src/Components/Counter/index.js
@@ -1,13 +1,29 @@
 import React from 'react';
+import { useState } from 'react';
+import { connect } from 'react-redux';
 
 const Counter = (props) => {
     return (
         <>
-            <h1>{value}</h1>
-            <button onClick={dec}>-</button>
-            <button onClick={inc}>+</button>
+            <h1>{props.value}</h1>
+            <button onClick={props.dec}>-</button>
+            <button onClick={props.inc}>+</button>
         </>
     )
 }
 
-export default Counter;
+const mapStateToProps = (state) => {
+    const {value} = state;
+    return {
+        value,
+    };
+};
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+        dec: () => dispatch({type: 'DEC'}),
+        inc: () => dispatch({type: 'INC'}),
+    }
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Counter);
